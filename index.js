856f208ba6582380a0daf8aa3d7e96ffb54970aa
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-03-26 12:45:08 +0530

    Backend: Initial Server Setup
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

---
+++ index.js
@@ -0,0 +1,48 @@
+// Initial Setup
+
+let express = require("express");
+let app = express();
+
+let mongoose = require("mongoose");
+
+let port = process.env.PORT || 4600;
+let morgan = require("morgan");
+
+let path = require("path");
+
+let config = require("config");
+process.env.NODE_CONFIG_DIR = "./config";
+let cors = require("cors");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
+
+// Enviornment Variables
+
+console.log(`Environment Mode: ${app.get("env")}`);
+console.log(`Development: ${config.get("info")}`);
+console.log(`Info: ${config.get("default")}`);
+console.log(`Enviornment Key : ${config.get("SecretKey")}`);
+
+// Check Secret key
+
+if (!config.get("SecretKey")) {
+  console.log(`Fatal Error: Set Key first`);
+  process.exit(1);
+}
+
+// Mongo DB connection
+
+mongoose
+  .connect("mongodb://localhost/MongooseDB-1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log(`Connection successful to Database`))
+  .catch(ex =>
+    console.log(`Something went wrong with, please check:s ${ex.message}`)
+  );
+
+app.listen(port, () => console.log(`Connection Successfull to ${port}`));
