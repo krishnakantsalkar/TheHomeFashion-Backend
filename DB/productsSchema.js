beb8400c522c30ba5cdc048cfbef3d574d90f5bf
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-03-27 17:26:16 +0530

    Backend: Add Products Schema
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

---
+++ DB/productsSchema.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose");
+
+let productsSChemaData = new mongoose.Schema({
+  name: { type: String, max: 20 },
+  description: { type: String },
+  quantity: { type: Number },
+  price: { type: Number },
+  offerPrice: { type: Number },
+  isAvailable: { type: Boolean },
+  isTodayOffer: { type: Boolean },
+  category: { type: String },
+  isAdmin: { type: Boolean },
+  recordDate: { type: Date, default: Date.now() },
+  updatedDate: { type: Date, default: Date.now() }
+});
+
+let productsSchema = mongoose.model("products", productsSChemaData);
+
+module.exports = productsSchema;
